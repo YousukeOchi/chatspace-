8750d8b5e58997196d832aace7ec5759cee43333
YousukeOchi <yowyowyou6@gmail.com>
2018-09-26 14:02:20 +0900

    Edit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,9 +13,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800]
+  # process resize_to_fit: [800, 800]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
